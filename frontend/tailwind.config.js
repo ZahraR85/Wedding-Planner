c725d648b06f0f57092637945ee0ed14d0eaf2ae
Shabnam <zahra.reihani85@gmail.com>
2024-12-09 14:53:53 +0100

    Navbar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{js, jsx, ts, tsx}"],
   theme: {
-    extend: {},
+    extend: { colors: {
+      customBg: "#FAF9F5", 
+      btnLight: "#DBD084",
+      btnDark: "#DBC537",
+    },
+    backgroundImage: {
+      'custom-gradient': 'linear-gradient(90deg, rgba(118,154,110) 0%, rgba(31,66,32) 100%)',
+    },},
   },
   plugins: [],
 }
