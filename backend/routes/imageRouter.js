097068b63fa35007f9e1855b877ca1a9332d5719
Shabnam <zahra.reihani85@gmail.com>
2025-01-05 20:44:56 +0100

    venueadmin update price

--- backend/routes/imageRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { upload, uploadImages } from "../controllers/imageController.js";
-
-const router = express.Router();
-
-router.post("/", upload.array("images", 10), uploadImages);
-
-export default router;
