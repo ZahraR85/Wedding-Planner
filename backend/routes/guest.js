d85ebaeb930879e81b29686f64f9448ec301be4e
Azadeh <keshavarzjoud@gmail.com>
2025-01-07 11:38:32 +0100

    add guest answer

--- backend/routes/guest.js
+++ backend/routes/guest.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import {
   createGuest,
   getGuests,
@@ -6,12 +6,18 @@ import {
   updateGuest,
   deleteGuest,
   sendInvitation,
-} from '../controllers/guestController.js';
+  countYesGuests,
+} from "../controllers/guestController.js";
 
 const router = express.Router();
 
+// Define specific routes first
+router.get("/count-yes", countYesGuests); // Specific route for counting "Yes" answers
+
+// Define general routes
 router.route("/").get(getGuests).post(createGuest);
-router.route("/:id").get(getGuestById).put(updateGuest).delete(deleteGuest);
+router.route("/:id").get(getGuestById).put(updateGuest).delete(deleteGuest); // Dynamic route for guest by ID
+
 router.post("/send-invitation", sendInvitation);
 
 export default router;
