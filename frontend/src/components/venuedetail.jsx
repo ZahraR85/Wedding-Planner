adeb0352b0eacc5d46fea34ae2a056c750fda3e6
Shabnam <zahra.reihani85@gmail.com>
2025-01-13 14:19:40 +0100

    rename2

--- frontend/src/components/venuedetail.jsx
+++ frontend/src/components/venuedetail.jsx
@@ -1,5 +1,5 @@
 import { useParams, useNavigate } from "react-router-dom";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function VenueDetail() {
